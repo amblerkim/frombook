33d6c49bf48a542be41fb24e0cd6854e309331c7
seunghabaek <slug0326@naver.com>
2023-10-24 00:11:36 +0900

    add googleBooksList

---
+++ frombook/src/app/data/googleBooksJson.js
@@ -0,0 +1,32 @@
+export const getGoogleBooksList = [
+  {
+    title: "상실의 시대",
+    bookId: "eIc1NAAACAAJ",
+    imgUrl:
+      "http://books.google.com/books/content?id=eIc1NAAACAAJ&printsec=frontcover&img=1&zoom=5&imgtk=AFLRE70s2dYcXUcWoHuXKQm9BSzeCOCaFCEwIJkwkNTacNSjkVsB9hKUqRDQ_JJN-WzxleRf6m0JyRbt4HnsyijmaU0vszsbAFE5a76wrRKDVsqAKGmiNQFKFet7P2BxKbpPmL8cjeKG&source=gbs_api",
+  },
+  {
+    title: "퓨쳐 셀프",
+    bookId: "CaXTEAAAQBAJ",
+    imgUrl:
+      "http://books.google.com/books/content?id=CaXTEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+  },
+  {
+    title: "도시와 그 불확실한 벽",
+    bookId: "GE3VEAAAQBAJ",
+    imgUrl:
+      "http://books.google.com/books/content?id=GE3VEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+  },
+  {
+    title: "비행운",
+    bookId: "b6ibywEACAAJ",
+    imgUrl:
+      "http://books.google.com/books/content?id=b6ibywEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+  },
+  {
+    title: "여자 없는 남자들",
+    bookId: "RSrbDwAAQBAJ",
+    imgUrl:
+      "http://books.google.com/books/content?id=RSrbDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+  },
+];
