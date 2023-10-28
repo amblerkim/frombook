d3387ac06eebbb4f3737edef2859a75a40f2b869
seunghabaek <slug0326@naver.com>
2023-10-28 23:43:36 +0900

    add your book review list

--- frombook/src/app/data/googleBooksJson.js
+++ frombook/src/app/data/googleBooksJson.js
@@ -1,32 +1,91 @@
-export const getGoogleBooksList = [
+// for 나의 기록
+export const getMyBookReviewList = [
   {
+    reviewNo: 1,
+    reviewer: "seungha.baek",
     title: "상실의 시대",
     bookId: "eIc1NAAACAAJ",
     imgUrl:
       "http://books.google.com/books/content?id=eIc1NAAACAAJ&printsec=frontcover&img=1&zoom=5&imgtk=AFLRE70s2dYcXUcWoHuXKQm9BSzeCOCaFCEwIJkwkNTacNSjkVsB9hKUqRDQ_JJN-WzxleRf6m0JyRbt4HnsyijmaU0vszsbAFE5a76wrRKDVsqAKGmiNQFKFet7P2BxKbpPmL8cjeKG&source=gbs_api",
   },
   {
+    reviewNo: 3,
+    reviewer: "seungha.baek",
     title: "퓨쳐 셀프",
     bookId: "CaXTEAAAQBAJ",
     imgUrl:
       "http://books.google.com/books/content?id=CaXTEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
   },
   {
+    reviewNo: 5,
+    reviewer: "seungha.baek",
     title: "도시와 그 불확실한 벽",
     bookId: "GE3VEAAAQBAJ",
     imgUrl:
       "http://books.google.com/books/content?id=GE3VEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
   },
   {
+    reviewNo: 7,
+    reviewer: "seungha.baek",
     title: "비행운",
     bookId: "b6ibywEACAAJ",
     imgUrl:
       "http://books.google.com/books/content?id=b6ibywEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
   },
   {
+    reviewNo: 9,
+    reviewer: "seungha.baek",
     title: "여자 없는 남자들",
     bookId: "RSrbDwAAQBAJ",
     imgUrl:
       "http://books.google.com/books/content?id=RSrbDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+  },
+];
+
+// for 타인의 기록
+export const getYourBookReviewList = [
+  {
+    reviewNo: 11,
+    reviewer: "hanseop.kim",
+    categories: "소설",
+    title: "정글만리",
+    imgUrl:
+      "http://books.google.com/books/content?id=9ZXEAwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+    author: "조정래",
+    publisher: "Hainaim Publishing",
+    registTime: "2023-10-02",
+  },
+  {
+    reviewNo: 13,
+    reviewer: "hanseop.kim",
+    categories: "소설",
+    title: "개미",
+    imgUrl:
+      "http://books.google.com/books/content?id=3FC5twAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
+    author: "Bernard Werber",
+    publisher: "문학동네",
+    registTime: "2023-09-12",
+  },
+  {
+    reviewNo: 15,
+    reviewer: "soyoung.park",
+    categories: "시",
+    title: "기사단장 죽이기 2",
+    imgUrl:
+      "http://books.google.com/books/content?id=aCvbDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+    author: "무라카미 하루키",
+    publisher: "문학동네",
+    registTime: "2023-10-01",
+  },
+  {
+    reviewNo: 17,
+    reviewer: "soyoung.park",
+    categories: "에세이",
+    title: "여자 없는 남자들",
+    imgUrl:
+      "http://books.google.com/books/content?id=RSrbDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api",
+    author: "무라카미 하루키",
+    publisher: "문학동네",
+    registTime: "2023-11-01",
   },
 ];
